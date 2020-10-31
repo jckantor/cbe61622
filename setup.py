0140125aa83bd68373ad94ac350f81008f8200f5
Jeffrey Kantor <kantor.1@nd.edu>
2020-10-31 08:31:02 -0400

    another try

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name = 'cbelaboratory',
-    packages = find_packages('src'),
     package_dir = {'': 'src'},
+    packages = [''],
 )
