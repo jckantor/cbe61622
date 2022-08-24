1de4f42ccf0607bad44e919e3281914fa7f9c60e
Jeffrey Kantor <kantor.1@nd.edu>
2022-08-24 07:23:40 -0400

    update org

---
+++ _build/jupyter_execute/notebooks/01/01-Configuring-a-Workstation.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Configuring a Workstation
+
+# ## Student Workstation
+# 
+# The basic workstation for this course provides a student with the means to learn the foundations of analog instrumentation, digital interfacing, and enough experience to pursue independent projects.
+
+# ## Laboratory Workstation
+# 
+# * Digilent Analog Studio
+# * Raspberry Pi Pico with Grove interface board
+# * Raspberry Pi 400 w/monitor
+# * Analog Devices ADALP2000 kit
+# * Stepper Motors
+# * Servo Motors
+# * Raspberry Pi Camera (Arducam)
+# 
+
+# ## Raspberry Pi
+
+# ### Assemble a Raspberry Pi Workstation
+# 
+# * Raspberry Pi 4 Model B (4GB)
+# * Raspberry Pi 7" touchscreen
+# * Raspberry Pi USB-C power supply
+# * Raspberry Pi USB keyboard and mouse
+# * SmartPi Touch 2 Case
+# * 32GB micro SD
+# 
+
+# ### Install Raspberry Pi OS
+# 
+# * Format a 16GB+ micro SD card using [SD Card Formatter](https://www.sdcard.org/downloads/formatter/)
+# * Install Raspberry Pi OS onto micro SD card using [Raspberry Pi Imager](https://www.raspberrypi.org/software/)
+
+# ## Digilent Analog Discovery Studio
+
+# ## Analog Devices Parts Kit ADALP2000
+# 
+# 
+# ### IC's
+# 
+# #### Amplifiers
+# 
+# * [AD8226 Instrumentation amp](https://www.analog.com/en/products/ad8226.html)
+# * [ADTL082 JFET op amp](https://www.analog.com/en/products/adtl082.html)
+# * [AD8542 dual CMOS dual rail-to-rail op amp](https://www.analog.com/en/products/ad8542.html)
+# * [OP482 high speed JFET quad op amp](https://www.analog.com/en/products/op482.html) (2)
+# * [OP484 precision rail-to-rail I/O quad op amp](https://www.analog.com/en/products/op484.html)
+# * [OP27 low noise, precision op amp](https://www.analog.com/en/products/op27.html) (2)
+# * [OP37 precision op amp](https://www.analog.com/en/products/op37.html) (2)
+# * [OP97 low power, high precision](https://www.analog.com/en/products/op97.html) (2)
+# * [LTC1541 micropower amp, comparator, reference](https://www.analog.com/en/products/ltc1541.html)
+# 
+# #### Sensors
+# 
+# * [AD8210 current shunt monitor](https://www.analog.com/en/products/ad8210.html)
+# * [AD22151 magnetic field sensor](https://www.analog.com/en/products/ad22151g.html)
+# * [ADXL327 3-axis accelerometer](https://www.analog.com/en/products/adxl327.html)
+# * [AD22100 voltage temperature sensor](https://www.analog.com/en/products/ad22100.html) (2)
+# * [AD592 current temperature sensor](https://www.analog.com/en/products/ad592.html)
+# * [TMP01 temperature controller](https://www.analog.com/en/products/tmp01.html)
+# * Electret microphone
+# * 10 kOhm thermister
+# * [OP999 PIN photodiode](https://www.ttelectronics.com/TTElectronics/media/ProductFiles/Optoelectronics/Datasheets/OP993-999.pdf)
+# 
+# #### Power
+# 
+# * [LT3092 programmable current source](https://www.analog.com/en/products/lt3092.html)
+# * [LTM8067 isolated dc-to-dc converter](https://www.analog.com/en/products/ltm8067.html)
+# * [LT3080 adjustable 1.1A LDO](https://www.analog.com/en/products/lt3080.html)
+# 
+# 
+# #### Data Conversion
+# 
+# * [AD654 voltage-to-frequency converter](https://www.analog.com/en/products/ad654.html)
+# * [AD5626 12-bit nanoDAC](https://www.analog.com/en/products/ad5626.html)
+# * [AD7920 12-bit 250kSPS SPI ADC](https://www.analog.com/en/products/ad7920.html)
+# 
+# #### Logic
+# 
+# * [SN74HC273 octal flip-flop](https://www.ti.com/product/SN74HC273)
+# 
+# #### Misc
+# 
+# ### Transistors
+# 
+# * [2N3904 general purpose NPN BJT](https://www.onsemi.com/pdf/datasheet/2n3903-d.pdf)
+# * [2N3906 general purpose PNP BJT](https://www.onsemi.com/pdf/datasheet/2n3906-d.pdf)
+# * [IRF510 power MOSFET](https://www.vishay.com/docs/91015/sihf510.pdf)
+# * [TIP31CFS NPN Power](https://alltransistors.com/adv/pdfdatasheet_st/tip31c.pdf)
+# * [TIP32CFS PNP Power](https://alltransistors.com/adv/pdfdatasheet_st/tip32c.pdf)
+# * [ZVN2110A N-channel DMOS FED]()v
+# 
+# 
+# 
+
+# ## Additional Components
+# 
+# Motors and Actuators
+# 
+# * [28BJY-48 Stepper Motors and ULN2003 Driver Board](https://www.amazon.com/ELEGOO-28BYJ-48-ULN2003-Stepper-Arduino/dp/B01CP18J4A) These very low-cost, ubiquitous stepper motors can be run from a 5 volt power supply supplied from a USB powered device. With the Raspberry Pi Pico, for example, the VBUS or VSYS pins can supply the required power. The ULN2003 driver board can handle up to 12 volt power for applications required more torque. 
+# 
+# * Servo Motor
+# 
+# 
+
+# In[ ]:
+
+
+
+
