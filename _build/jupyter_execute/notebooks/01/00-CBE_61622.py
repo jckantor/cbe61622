1ff5782c7b3bdd18fd1205142ec1d6a6e5e5c83c
Jeffrey Kantor <kantor.1@nd.edu>
2022-08-30 15:07:27 -0400

    updates

---
+++ _build/jupyter_execute/notebooks/01/00-CBE_61622.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # CBE 61622
+
+# In[ ]:
+
+
+
+
---
+++ _build/jupyter_execute/notebooks/02/00-Raspberry-Pi-Pico.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Raspberry Pi Pico
+# 
+# The Raspberry Pi Pico is a recent example of a low-cost microcontroller well suited to laboratory automation and data collection. This series of notebooks is designed to get you started with coding in MicroPython for the Raspberry Pi Pico, working with serial interfaces, and performing basic analog and digital measurements and controls.
+
+# In[ ]:
+
+
+
+
