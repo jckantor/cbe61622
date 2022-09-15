a31dd79eb7fd236c7a7ce0cb9d2fe8927b307800
Jeffrey Kantor <kantor.1@nd.edu>
2022-09-15 14:50:19 -0400

    rebuild

--- _build/jupyter_execute/notebooks/03/00-Analog-Circuits-and-Signals.py
+++ _build/jupyter_execute/notebooks/03/00-Analog-Circuits-and-Signals.py
@@ -17,13 +17,18 @@
 #     * Experiment: Measure noise in an MEMS sensor (3-axis acc.)
 #     * Experiment: Measure elecrochemical noise of solution
 #     * Experiment: Meauure noise in an image sensor (IR sensor?)
+#  
 #     
 # 
-#     
+
+# ## Reference Materials
 # 
-
-# In[ ]:
-
-
-
-
+# * Hayes, Thomas C. (2016). Learning the Art of Electronics: A Hands-on Lab Course. Cambridge University Press, 2016. A Hands-On Lab Course 1st Edition. (https://www.uvm.edu/~gpetrucc/courses/Chem219/Lectures/%5BTom_Hayes%3B_Paul_Horowitz%5D_Learning_the_Art_of_Ele(z-lib.org).pdf)
+# 
+# * Monk, S. (2017). Electronics Cookbook: Practical Electronic Recipes with Arduino and Raspberry Pi. " O'Reilly Media, Inc.".
+# 
+# * Bohn, Paul. Electronics for Chemical Instrumentation.
+# 
+# 
+# 
+# 
--- _build/jupyter_execute/notebooks/03/02-Operational-Amplifiers.py
+++ _build/jupyter_execute/notebooks/03/02-Operational-Amplifiers.py
@@ -3,22 +3,59 @@
 
 # # Operational Amplifiers
 
-# (Harvey Mudd) ![](http://fourier.eng.hmc.edu/e84/lectures/figures/OpAmp0.gif)
-# 
-# * [Input impedance]
-# * [Output impedance]
-# * [Open-circuit gain]
-# 
-# An approximate equation for the output voltage is
-# 
-# $$V_{out} = A_d (V_+ - V_-) + A_c(V_+ + V_-)$$
-# 
-# where $A_d$ is the differential gain and $A_c$ is the common-mode gain. Normally we seek very high values for $A_d$ and very low values for $A_c$. The common-mode rejection ratio is
-# 
-# $$CMRR = 20 \log_{10}\frac{A_d}{A_c}$$
-# 
-# Typical values of $CCMR$ are 100db or greater indicating $A_d > 10^5A_c$
+# ## Readings
 
+# ## Laboratory Exercises
+# 
+# ### 1. Familiarization with Operational Amplifiers
+# 
+# 1. In the Analog Devices ADALP2000 kit, locate
+#      * OP27 Low Noise, Precision Operational Amplifier
+#      * OP37 Precision Operational Amplifier
+#      * OP97 Low Noise, Precision Operational Amplifier
+#      * OP482 High Speed JFET Op Amp
+#      * OP484 Precision Rail-to-Rail Op Amp
+#      * AD8226 Instrumentation Amplifier
+#      * AD8542 CMOS Rail-to-Rail Op Amp
+#      * ADLT082 JFET Op Amp
+#      
+# 2. For each Op Amp above, locate (2) data sheet and (2) unit price. For each, write one sentence describing describing what makes that op amp unique from the others. 
+# 
+# ### 2. Laboratory Testing
+# 
+# 1. Using the OP27
+#     * Setup +/- 12 volt power
+#     * Use a 10K potentiometer to create an adjustable voltage divider. Use as the input to the Op-amp according to Fig 8.7 and 8.8 of the text. Can you estimate the gain $A_0$?
+#     
+#     
+# 2. Buffer Circuit (Unity Gain)
+# 
+# 3. Inverting Amplifier
+# 
+# 4. Non-inverting Amplifier
+# 
+# 5. Current-to-Voltage Converter
+#     * Locate the phototransistor in the ADALP2000 kit. Locate the data sheet. Set up a current to voltage converter (see Figure 8.41 for ideas) and verify measurement of a light source.
+# 
+#      
+#      
+# Ignore this cell.  Just some edit notes(Harvey Mudd) 
+
+![Op](http://fourier.eng.hmc.edu/e84/lectures/figures/OpAmp0.gif)
+
+* [Input impedance]
+* [Output impedance]
+* [Open-circuit gain]
+
+An approximate equation for the output voltage is
+
+$$V_{out} = A_d (V_+ - V_-) + A_c(V_+ + V_-)$$
+
+where $A_d$ is the differential gain and $A_c$ is the common-mode gain. Normally we seek very high values for $A_d$ and very low values for $A_c$. The common-mode rejection ratio is
+
+$$CMRR = 20 \log_{10}\frac{A_d}{A_c}$$
+
+Typical values of $CCMR$ are 100db or greater indicating $A_d > 10^5A_c$
 # ## Feedback
 
 # In[ ]:
