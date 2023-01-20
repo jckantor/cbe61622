17e954d455eecc987c72de18c7988b5b5bc554cd
Jeffrey Kantor <kantor.1@nd.edu>
2023-01-20 18:21:26 -0500

    add microscope

---
+++ _build/jupyter_execute/notebooks/05/05-microscopy-concept.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # RPi Microscopy Ideas
+# 
+# ## Project Objectives
+# 
+# * Low cost microscope for laboratory applications.
+# 
+# 
+# ## Commercial Devices
+# 
+# Lens Options for C-mount HQ Camera
+# 
+# * Pimoroni .. https://shop.pimoroni.com/products/microscope-lens-0-12-1-8x?variant=31885093371987. Review available here https://www.tomshardware.com/reviews/pimoroni-microscope-lens-for-raspberry-pi and https://www.youtube.com/watch?v=_lTzv5IwWNk&ab_channel=PenguinTutor
+# * Seeed  https://www.electromaker.io/shop/product/microscope-camera-300x-c-mount-lens-for-raspberry-pi-high-quality-camera
+# 
+# Stands
+# 
+# * Pimoroni .. https://shop.pimoroni.com/products/microscope-stand-with-0-12-1-8x-lens?variant=31885089046611
+# * Adafruit lens mount for Pi-Zero and RPI 4
+# * Waveshare .. https://www.waveshare.com/pi4-microscope-kit.htm
+# * Hayear .. https://www.amazon.com/HAYEAR-Industry-Stereo-Digital-Microscope/dp/B07GPCQWXK
+# 
+# 
+# ## Software Options
+# 
+# * USB Web Cam would enable simple "plug-and-play" microscope use. https://github.com/geerlingguy/pi-webcam
+# 
+# 
